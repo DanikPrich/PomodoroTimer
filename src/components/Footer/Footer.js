f0a76d71ef27908fe17c83f9aedd21fd6960db38
Daniel <Danil.danek@gmail.com>
2023-05-02 20:35:25 +0200

    pause feature

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import './footer.scss'
 import resetImg from '../../resources/img/reset.svg'
 
-const Footer = ({onStartPressed, onResetPressed}) => {
+const Footer = ({onStartPressed, onPausePressed, onResetPressed, timerRunning, timerPaused}) => {
   return (
     <div className="pomodoro__footer">
-      <button onClick={onStartPressed} className="pomodoro__btn pomodoro__start">
+      <button onClick={onStartPressed} className={`pomodoro__btn ${timerRunning ? 'pomodoro__btn_active' : ''}`}>
         start
+      </button>
+      <button onClick={onPausePressed} className={`pomodoro__btn ${timerPaused ? 'pomodoro__btn_active' : ''}`}>
+        pause
       </button>
       <img onClick={onResetPressed} className="pomodoro__reset" src={ resetImg } alt="reset button"></img>
     </div>
